5350e525cdef3f8f85015f1ed540cb45ee7f97cf
pro-masud <promasudbd@gmail.com>
2025-10-22 17:00:33 +0600

    feat: header section design complete

---
+++ elementor/addons/elementor-view/about-one.php
@@ -0,0 +1,44 @@
+<?php if ('layout_one' === $settings['layout_type']) : ?>
+    <header class="header-area" >
+        <div class="cozmiq-border">
+            <div class="cozmiq-mid-border">
+                <div class="cozmiq-bg"></div>
+            </div>
+        </div>
+        <div class="cozmiq-container">
+            <div class="cozmiq-bg-img">
+                <img src="https://framerusercontent.com/images/AVFyLS6QiN23juDQx8Ih2TFqY.png" alt="">
+            </div>
+            <div class="bg-overlay"></div>
+            <div class="header-wrapper">
+                <div class="cozmiq-user">
+                    <div class="cozmiq-user-img">
+                        <img src="https://framerusercontent.com/images/2oSRzohxW6QjKgtlTllhPoSuGrg.jpg" alt="this is user image">
+                    </div>
+                    <div class="cozmiq-user-info">
+                        <h5>Mike Jonson</h5>
+                        <p><span></span>Available for work</p>
+                    </div>
+                </div>
+                <div class="cozmiq-menu">
+                    <ul>
+                        <li>
+                            <a href="#about"><img src="https://framerusercontent.com/images/NZRTyB2IHRikl3tzpkupcI5nk.svg" alt=""></a>
+                            <span>About</span>
+                        </li>
+                        <li><a href="#skills"><img src="https://framerusercontent.com/images/DKZj8f9stHVQbLBmsG02WQvTvk.svg" alt=""></a></li>
+                        <li><a href="#experience"><img src="https://framerusercontent.com/images/fhcXyT2dBmVTBXHQkQVzOR8UN4.svg" alt=""></a></li>
+                        <li><a href="#education"><img src="https://framerusercontent.com/images/NZRTyB2IHRikl3tzpkupcI5nk.svg" alt=""></a></li>
+                        <li><a href="#portfolio"><img src="https://framerusercontent.com/images/zkOQ9IQxT4OfCSeTYYdeou6VVmo.svg" alt=""></a></li>
+                        <li><a href="#contact"><img src="https://framerusercontent.com/images/tJzUFHAtazIBDgdRDMtelp1gNnk.svg" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="cozmiq-border">
+            <div class="cozmiq-mid-border">
+                <div class="cozmiq-bg"></div>
+            </div>
+        </div>
+    </header>
+<?php endif; ?>
